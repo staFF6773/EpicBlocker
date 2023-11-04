e3d2dd70fc448234702032c35eea4bb57542fa14
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2023-11-03 22:27:42 -0600

    Update playerblock.java

--- src/main/java/block/playerblock.java
+++ src/main/java/block/playerblock.java
@@ -5,11 +5,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.Plugin;
 import utils.ChatUtils;
 
 public class playerblock implements Listener {
 
-        private final EpicBlocker plugin;
+    private final EpicBlocker plugin;
 
     public playerblock(EpicBlocker plugin) {
         this.plugin = plugin;
@@ -19,6 +20,11 @@ public class playerblock implements Listener {
     public void onChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         String message = event.getMessage();
+
+        // Verifica si el jugador tiene el permiso de bypass
+        if (player.hasPermission("epicblocker.bypass")) {
+            return;  // No se aplica el bloqueo
+        }
 
         for (String blockedWord : plugin.getConfig().getStringList("blocked-words")) {
             if (message.toLowerCase().contains(blockedWord.toLowerCase())) {
