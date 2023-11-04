5221239e4ac3bbfdebcf8cae100bba5b6a0a119e
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2023-11-03 22:16:02 -0600

    Update reload.java

--- src/main/java/commands/reload.java
+++ src/main/java/commands/reload.java
@@ -20,16 +20,23 @@ public class reload implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
         if (sender.hasPermission("epicblocker.reload") || sender instanceof ConsoleCommandSender || sender.isOp()) {
             reloadConfig();
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cEpicBlocker &7» &aThe configuration file was reloaded."));
             sender.sendMessage(ChatUtils.getColoredMessage("&7(Some options only apply after the server has been restarted.)"));
         } else {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cEpicBlocker &7» &c&lUPS &cSorry, but you do not have permission to execute this command."));
+            // Obtén el mensaje desde la configuración
+            String noPermissionMessage = plugin.getConfig().getString("no-permission-message");
+            // Si no se encuentra el mensaje en la configuración, usa uno por defecto
+            if (noPermissionMessage == null) {
+                noPermissionMessage = "&c&lUPS &cSorry, but you do not have permission to execute this command.";
+            }
+            // Envía el mensaje de falta de permisos
+            sender.sendMessage(ChatUtils.getColoredMessage(EpicBlocker.prefix + " " + noPermissionMessage));
         }
         return true;
     }
+
 
     private void reloadConfig() {
         File configFile = new File(plugin.getDataFolder(), "config.yml");
